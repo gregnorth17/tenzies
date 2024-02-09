d71ff2c59b84b00d929b7083313844405cd169f0
gregnorth17 <greg_north@hotmail.co.uk>
2024-02-09 17:19:06 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import Die from './Die'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className='container'>
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+          <Die value={1} />
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/Die.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const Die = ({value}) => {
+  return (
+    <div className='die'>
+      {value}
+    </div>
+  )
+}
+
+export default Die
