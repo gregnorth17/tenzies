f56a8af627670c3cad2ff3adcebfee6ca75320ca
gregnorth17 <greg_north@hotmail.co.uk>
2024-02-14 13:55:41 -0600

    win game

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { nanoid } from 'nanoid'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Die from './Die'
 
 function App() {
 
-  const allNewDice = () => {
-    const newDice = []
-    for(let i = 0; i < 10; i++) {
-      newDice.push(createDice())
-    }
-    return newDice
-  }
+  const [tenzies, setTenzies] = useState(false)
+  const [dice, setDice] = useState(allNewDice())
+
   
-  const createDice = () => {
+  useEffect(() => {
+    setTenzies(dice.every(die => die.isHeld && die.value === dice[0].value))
+    // console.log(tenzies)
+    tenzies && console.log('you won')
+  }, [dice, tenzies])
+
+  function createDice () {
     return {
       value: Math.ceil(Math.random() * 6),
       isHeld: false,
@@ -21,9 +23,15 @@ function App() {
     }
   }
 
-  const [dice, setDice] = useState(allNewDice())
-
-  const rollDice = () => {
+  function allNewDice() {
+    const newDice = []
+    for(let i = 0; i < 10; i++) {
+      newDice.push(createDice())
+    }
+    return newDice
+  }
+  
+  function rollDice() {
     setDice(oldDice => oldDice.map(dice => {
       return dice.isHeld ?
         dice :
@@ -31,7 +39,7 @@ function App() {
     }))
   }
 
-  const holdDice = id => {
+  function holdDice(id) {
     setDice(oldDice => oldDice.map(dice => {
       return dice.id === id ?
           {...dice, isHeld: !dice.isHeld} :
@@ -39,11 +47,19 @@ function App() {
     }))
   }
 
-  const diceElements = dice.map(({value, isHeld, id}) => <Die holdDice={() => holdDice(id)} key={id} id={id} isHeld={isHeld} value={value} />)
+  const diceElements = dice.map(({value, isHeld, id}) => 
+    <Die holdDice={() => holdDice(id)} 
+         key={id} 
+         id={id} 
+         isHeld={isHeld} 
+         value={value} 
+     />)
 
   return (
     <>
       <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='container'>
           {diceElements}
         </div>
