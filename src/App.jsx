dbcded76e68b80ce6fc13cac302a16d7947cc3fd
gregnorth17 <greg_north@hotmail.co.uk>
2024-02-22 15:00:37 -0600

    confetti window

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useWindowSize } from "@uidotdev/usehooks"
 import { nanoid } from 'nanoid'
 import { useEffect, useState } from 'react'
 import Confetti from 'react-confetti'
@@ -12,9 +13,11 @@ function App() {
 
   const [tenzies, setTenzies] = useState(false)
   const [finalTime, setFinalTime] = useState(null)
-  const [dice, setDice] = useState(allNewDice())
+  const [dice, setDice] = useState([0,0,0,0,0,0,0,0,0,0])
   const [numberOfRolls, setNumberOfRolls] = useState(0)
   const [isRunning, setIsRunning] = useState(false)
+
+  const { width, height } = useWindowSize()
 
   const {
     totalSeconds,
@@ -75,7 +78,7 @@ function App() {
       setDice(allNewDice())
       setIsRunning(true)
       setFinalTime(0)
-    }, 500)
+    }, 1000)
   }
 
   function holdDice(id) {
@@ -99,7 +102,7 @@ function App() {
   return (
     <>
       <main>
-        {tenzies && <Confetti />}
+        {tenzies && <Confetti width={width} height={height} />}
         <h1 className='title'>Tenzies</h1>
         <p className='instructions'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='container'>
